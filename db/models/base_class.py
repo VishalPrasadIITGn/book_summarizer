e4d52b2f24099a5731cd091b40df36d267079d15
VishalPrasadIITGn <vishal.prasad@alumni.iitgn.ac.in>
2024-07-01 11:47:43 +0530

    removed double code

--- db/models/base_class.py
+++
@@ -1,5 +0,0 @@
-"""Creating base class."""
-
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
--- utils/exception_utils.py
+++
@@ -1,4 +0,0 @@
-"""Contains exceptions and error."""
-
-class AlreadyExistsError(Exception):
-  """This is raised when file already exists in database."""
