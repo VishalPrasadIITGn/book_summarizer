7510e75236deda1b95ef5eb79d4b9c0d46dc227f
VishalPrasadIITGn <vishal.prasad@alumni.iitgn.ac.in>
2024-07-01 17:21:37 +0530

    updated README

--- README.md
+++ README.md
@@ -5,8 +5,11 @@ books management system with summarizer using llama3
 git clone https://github.com/VishalPrasadIITGn/book_summarizer.git
 run `cd book_summarizer`
 run `poetry install`
-run either of two commands:
-1. `python book_summarizer/recommendation/prediction/make_prediction.py`
+run `cd book_summarizer/db_migration`
+run `alembic upgrade head`
+run `cd ../..`
+to host the API, run either of two commands:
+1. `python book_summarizer/api/main.py`
 2. `gunicorn book_summarizer.api.main:app --bind 0.0.0.0:8000 --workers 4 --max-requests 2000 --max-requests-jitter 100 --worker-class uvicorn.workers.UvicornWorker --graceful-timeout 100 --reload`
 access localhost:8000 to see the swagger
 
--- book_summarizer/db/models/models.py
+++ book_summarizer/db/models/models.py
@@ -13,87 +13,14 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
 from sqlalchemy.orm import Mapped
-# from sqlalchemy.orm import mapped_column
 
 from book_summarizer.db.models.base_class import Base
 metadata = Base.metadata
 
-# class Books(Base):
-#   """Dataset class for entry to dataset table."""
 
-#   __tablename__ = "books"
-
-#   id: Mapped[int] = Column(Integer, index=True, unique=True, primary_key=True)
-#   title = Column(String, index=True)
-#   author = Column(String, index=True)
-#   genre = Column(String, index=True)
-#   summary = Column(String, index=True)
-#   year_published = Column(Integer, index=True)
-
-#   # book_review = relationship("Reviews",
-#   #                             back_populates="review_book",
-#   #                             cascade='all, delete',
-#   #                             foreign_keys="Reviews.id")
-#   reviews: Mapped[list["Reviews"]] = relationship("Reviews", back_populates="books", foreign_keys="Reviews.id")
-
-
-# class Reviews(Base):
-#   """Lease class for lease documents."""
-#   __tablename__ = "reviews"
-
-#   # id = Column(Integer, index=True, unique=True, primary_key=True)
-
-#   id: Mapped[int] = Column(index=True, primary_key=True, unique=True)
-#   # book_id: Mapped[int] = Column(ForeignKey("Books.id"))
-#   book_id: Mapped[int] = Column(ForeignKey("books.id"), index=True)
-#   books: Mapped["Books"] = relationship("Books", back_populates="reviews", foreign_keys="Books.id")
-
-#   # book_id = Column(Integer, ForeignKey("books.id"))
-#   user_id = Column(String, index=True)
-#   review_text = Column(String, index=True)
-#   rating = Column(Integer, index=True)
-
-#   # review_book = relationship("Books",
-#   #                           back_populates="book_review",
-#   #                           cascade='all, delete',
-#   #                           foreign_keys="Books.id")
-  
-"""SQLAlchemy ORM models for database."""
-
-"""SQLAlchemy ORM models for database."""
-
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, Mapped
-from book_summarizer.db.models.base_class import Base
-
-
-# class Books(Base):
-#     """Dataset class for entry to dataset table."""
-
-#     __tablename__ = "books"
-
-#     id: Mapped[int] = Column(Integer, index=True, unique=True, primary_key=True)
-#     title = Column(String, index=True, unique=True)
-#     author = Column(String, index=True)
-#     genre = Column(String, index=True)
-#     summary = Column(String, index=True)
-#     year_published = Column(Integer, index=True)
-
-#     reviews: Mapped[list["Reviews"]] = relationship("Reviews", back_populates="books", cascade='all, delete-orphan')
-
-
-# class Reviews(Base):
-#     """Lease class for lease documents."""
-
-#     __tablename__ = "reviews"
-
-#     id: Mapped[int] = Column(Integer, index=True, unique=True, primary_key=True)
-#     book_id: Mapped[int] = Column(ForeignKey("books.id"), index=True)
-#     user_id = Column(String, index=True)
-#     review_text = Column(String, index=True)
-#     rating = Column(Integer, index=True)
-
-#     books: Mapped["Books"] = relationship("Books", back_populates="reviews", foreign_keys="books.id")
+# from sqlalchemy import Column, ForeignKey, Integer, String
+# from sqlalchemy.orm import relationship, Mapped
+# from book_summarizer.db.models.base_class import Base
 
 
 class Books(Base):
