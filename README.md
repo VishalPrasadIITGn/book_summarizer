04c52de6bcfcc02fcb0d54deec3e1574ba1508e6
VishalPrasadIITGn <vishal.prasad@alumni.iitgn.ac.in>
2024-07-04 14:22:00 +0530

    updated config

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 books management system with summarizer using llama3
 
 ## Deploy using fastapi
-git clone https://github.com/VishalPrasadIITGn/book_summarizer.git
+run `git clone https://github.com/VishalPrasadIITGn/book_summarizer.git`
 
 run `cd book_summarizer`
 
@@ -10,33 +10,33 @@ run postgres database using the command `docker run -d -e POSTGRES_USER=user -e 
 
 run `poetry install`
 
-run `cd book_summarizer/db_migration`
+run `export DB_NAME=<database_name>`
 
-run `alembic upgrade head`
+run these commands in these order to ensure database is upgraded to latest model: `cd book_summarizer/db_migration`, then run `alembic upgrade head`
 
 run `cd ../..`
 
 to host the API, run either of two commands:
 1. `python book_summarizer/api/main.py`
 2. `gunicorn book_summarizer.api.main:app --bind 0.0.0.0:8000 --workers 4 --max-requests 2000 --max-requests-jitter 100 --worker-class uvicorn.workers.UvicornWorker --graceful-timeout 100 --reload`
-access localhost:8000 to see the swagger
+
+Access localhost:8000 to see the swagger
 
 ## Deploy using docker container
-git clone 
-
-https://github.com/VishalPrasadIITGn/book_summarizer.git
+run `git clone https://github.com/VishalPrasadIITGn/book_summarizer.git`
 
 run `cd book_summarizer`
+
+build the image `docker build --no-cache -t book_rec:v1 .`
 
 run the following command `docker run -p 8000:8000 book_rec:v1`
 
 ## Deploy using docker compose
-git clone 
-https://github.com/VishalPrasadIITGn/book_summarizer.git
+git clone `https://github.com/VishalPrasadIITGn/book_summarizer.git`
 
 run `cd book_summarizer`
 
-Build the image `docker build --no-cache -t book_rec:v1 .`
+build the image `docker build --no-cache -t book_rec:v1 .`
 
 run the command `docker-compose up`
 
--- book_summarizer/config/config.py
+++ book_summarizer/config/config.py
@@ -10,13 +10,7 @@ from book_summarizer.llm.prompts.book_prompt import book_summary_prompt
 class Settings(BaseSettings):
   """Environment variables."""
 
-  CLM_URL: str = "http://10.113.34.11:8999"
-  REDIS_HOST: str = "10.113.34.11"
-  REDIS_PORT: int = 6886
-  REDIS_PASSWORD: str = ""
-  REDIS_URL: str = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}"
-  OPENAI_COMPLETIONS_ENGINE: str = "gpt-3.5-turbo"
-  UPLOAD_PATH: str = f"uploaded_docs"
+
   # LLM_MODEL_NAME: str="meta-llama/Meta-Llama-3-8B-Instruct"
   LLM_MODEL_NAME: str = "microsoft/Phi-3-mini-128k-instruct"
   LLM_PROMPT: str = book_summary_prompt
@@ -27,10 +21,10 @@ class Settings(BaseSettings):
   DB_PASSWORD: str = os.getenv("DB_PASSWORD") or "password123"
   DB_NAME: str = "book_rec_2"
 
-  DB_HOST: str = "10.113.34.11"
+  DB_HOST: str = "0.0.0.0"
   DB_PORT: int = 8765
   DB_SCHEME: str = "postgresql"
-  SQLALCHEMY_DATABASE_URL: PostgresDsn | None = "postgresql://user:password123@10.113.34.11:8765/book_rec_2"
+  SQLALCHEMY_DATABASE_URL: PostgresDsn | None = "postgresql://user:password123@0.0.0.0:8765/book_rec_2"
 
   @field_validator("SQLALCHEMY_DATABASE_URL", mode="after")
   @classmethod
@@ -49,92 +43,3 @@ class Settings(BaseSettings):
 
 
 setting = Settings()
-#confi =Settings()
-# print("DB_USERNAME : ",setting.DB_USERNAME)
-# print("DB_PASSWORD : ",setting.DB_PASSWORD)
-# print("DB_PORT : ",setting.DB_PORT)
-# print("DB_SCHEME : ",setting.DB_SCHEME)
-# print("DB_HOST : ",setting.DB_HOST)
-
-# redis_cache = {
-#     "uploaded_docs":
-#     redis.StrictRedis( # type: ignore
-#         host=setting.REDIS_HOST,
-#         port=setting.REDIS_PORT,
-#         password=setting.REDIS_PASSWORD,
-#         decode_responses=True,
-#         db=1,
-#     ),
-#     "lease_data":
-#     redis.StrictRedis( # type: ignore
-#         host=setting.REDIS_HOST,
-#         port=setting.REDIS_PORT,
-#         password=setting.REDIS_PASSWORD,
-#         decode_responses=True,
-#         db=2,
-#     ),
-#     "parsed_results":
-#     redis.StrictRedis( # type: ignore
-#         host=setting.REDIS_HOST,
-#         port=setting.REDIS_PORT,
-#         password=setting.REDIS_PASSWORD,
-#         decode_responses=True,
-#         db=3,
-#     ),
-#     "document_qa":
-#     redis.StrictRedis( # type: ignore
-#         host=setting.REDIS_HOST,
-#         port=setting.REDIS_PORT,
-#         password=setting.REDIS_PASSWORD,
-#         decode_responses=True,
-#         db=4,
-#     ),
-#     "prompts":
-#     redis.StrictRedis( # type: ignore
-#         host=setting.REDIS_HOST,
-#         port=setting.REDIS_PORT,
-#         password=setting.REDIS_PASSWORD,
-#         decode_responses=True,
-#         db=5,
-#     ),
-#     "document_qa_user":
-#     redis.StrictRedis( # type: ignore
-#         host=setting.REDIS_HOST,
-#         port=setting.REDIS_PORT,
-#         password=setting.REDIS_PASSWORD,
-#         decode_responses=True,
-#         db=6,
-#     ),
-#     "lru_cache":
-#     redis.StrictRedis( # type: ignore
-#         host=setting.REDIS_HOST,
-#         port=setting.REDIS_PORT,
-#         password=setting.REDIS_PASSWORD,
-#         decode_responses=False,
-#         db=7,
-#     ),
-#     "lease_summary_data":
-#     redis.StrictRedis( # type: ignore
-#         host=setting.REDIS_HOST,
-#         port=setting.REDIS_PORT,
-#         password=setting.REDIS_PASSWORD,
-#         decode_responses=True,
-#         db=8,
-#     ),
-#     "invoice_data":
-#     redis.StrictRedis( # type: ignore
-#         host=setting.REDIS_HOST,
-#         port=setting.REDIS_PORT,
-#         password=setting.REDIS_PASSWORD,
-#         decode_responses=True,
-#         db=8,
-#     ),
-#     "reconciliation_history":
-#     redis.StrictRedis( # type: ignore
-#         host=setting.REDIS_HOST,
-#         port=setting.REDIS_PORT,
-#         password=setting.REDIS_PASSWORD,
-#         decode_responses=True,
-#         db=9,
-#     ),
-# }
